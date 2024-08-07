6533bdca27abac4d7d61f8b7e725a86526d9132c
brian-h2 <brianheredia200309@gmail.com>
2024-08-07 10:48:58 -0300

    Agregado de Data y Model necesarios para la conexion y manejo de datos

---
+++ Data/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using ApiMinima.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMinima.Data
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options ) 
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
---
+++ Model/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiMinima.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        //Clase que representa los datos que la aplicacion administra
+    }
+}
